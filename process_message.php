82bcef0a79125fff642d9d878d6c4b3c33fc7fe4
ZeenathLV <ZeenathLV@users.noreply.github.com>
2016-04-18 20:19:34 -0700

    Added files via upload
    Added process_message.php

---
+++ process_message.php
@@ -0,0 +1,23 @@
+<?php
+try
+{
+  $twillio_enabled = false;
+  $rider = new BusRider($twillio_enabled);
+  
+  $rider->load_rider($phone, $message);
+  
+  if($rider->has_error())
+     throw new Exception($rider->get_error());
+  
+  $rider->process_message();
+
+  if($rider->has_error())
+     throw new Exception($rider->get_error());
+  
+}
+catch(Exception $e)
+{
+  echo $e->getMessage();
+}
+
+?>             
